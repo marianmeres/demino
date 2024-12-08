c3b2846450e13a06e5884c860b36fec069f01b4e
Marian Meres <marian@meres.sk>
2024-12-08 23:25:50 +0100

    types fix

--- src/utils/is-valid-date.ts
+++ src/utils/is-valid-date.ts
@@ -1,6 +1,6 @@
 // deno-lint-ignore-file no-explicit-any
 
 /** Checks if value is a valid date */
-export function isValidDate(v: any) {
+export function isValidDate(v: any): boolean {
 	return v instanceof Date && !isNaN(v.getTime());
 }
