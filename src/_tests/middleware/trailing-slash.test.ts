8856ca947eaf867ee981b0f3f8d87e99b334fde6
Marian Meres <marian@meres.sk>
2024-12-09 09:13:25 +0100

    trailing slash improved

--- src/_tests/middleware/trailing-slash.test.ts
+++ src/_tests/middleware/trailing-slash.test.ts
@@ -1,3 +1,6 @@
+// deno-lint-ignore-file no-explicit-any
+
+import { assertEquals } from "@std/assert";
 import { demino } from "../../demino.ts";
 import { createTrailingSlashMiddleware } from "../../middleware/trailing-slash.ts";
 import { assertResp, startTestServer } from "../_utils.ts";
@@ -11,10 +14,15 @@ Deno.test("trailing slash manual redirect check", async () => {
 		const tsOn = createTrailingSlashMiddleware(true);
 		const tsOff = createTrailingSlashMiddleware(false);
 
+		const globalMwOutput =
+			"this must not be reached (tsMw will be executed earlier)";
 		const app = demino();
-		app.use(() => "this must not be reached because ts will be sorted earlier");
+		app.use(() => globalMwOutput);
 		app.get("/foo/bar", tsOn, () => "foo");
 		app.get("/baz/bat", tsOff, () => "baz");
+
+		// root must be ignored
+		app.get("/", tsOff, () => "home");
 
 		srv = await startTestServer(app);
 
@@ -33,6 +41,13 @@ Deno.test("trailing slash manual redirect check", async () => {
 			"",
 			{ location: /[^\/]$/ }
 		);
+
+		// NO-OP (it's correct that the global ms is reached here)
+		await assertResp(
+			fetch(`${srv.base}`, { redirect: "manual" }),
+			200,
+			globalMwOutput
+		);
 	} catch (e) {
 		throw e;
 	} finally {
@@ -45,9 +60,11 @@ Deno.test("trailing slash manual redirect check", async () => {
 Deno.test("trailing slash auto redirect", async () => {
 	let srv: Srv | null = null;
 
+	const _log: string[] = [];
+	const logger = (v: string) => _log.push(v);
 	try {
-		const tsOn = createTrailingSlashMiddleware(true);
-		const tsOff = createTrailingSlashMiddleware(false);
+		const tsOn = createTrailingSlashMiddleware(true, { logger });
+		const tsOff = createTrailingSlashMiddleware(false, { logger });
 
 		const app = demino();
 		app.get("/foo/bar", tsOn, () => "foo");
@@ -57,11 +74,14 @@ Deno.test("trailing slash auto redirect", async () => {
 
 		// ON
 		await assertResp(fetch(`${srv.base}/foo/bar`), 200, "foo");
-		await assertResp(fetch(`${srv.base}/foo/bar/`), 200, "foo");
+		await assertResp(fetch(`${srv.base}/foo/bar/`), 200, "foo"); // no-op
 
 		// OFF
 		await assertResp(fetch(`${srv.base}/baz/bat/`), 200, "baz");
-		await assertResp(fetch(`${srv.base}/baz/bat`), 200, "baz");
+		await assertResp(fetch(`${srv.base}/baz/bat`), 200, "baz"); // no-op
+
+		// the above 4 fetches must have triggered exactly 2 redirects (2 were no-ops)
+		assertEquals(_log.length, 2);
 	} catch (e) {
 		throw e;
 	} finally {
--- src/middleware/trailing-slash.ts
+++ src/middleware/trailing-slash.ts
@@ -1,4 +1,3 @@
-import { moveSync } from "@std/fs/move";
 import type { DeminoContext, DeminoHandler } from "../demino.ts";
 
 /**
@@ -11,20 +10,30 @@ import type { DeminoContext, DeminoHandler } from "../demino.ts";
  * If there is nothing to do, will do nothing. This middleware will try to position itself
  * at the beginning of the middlewares stack, to potentially terminate the chain ASAP.
  */
-export function createTrailingSlashMiddleware(flag: boolean): DeminoHandler {
+export function createTrailingSlashMiddleware(
+	/** The master flag - TRUE: add slash, FALSE: remove slash */
+	flag: boolean,
+	options?: {
+		/** For debugging */
+		logger?: CallableFunction;
+	}
+): DeminoHandler {
 	const midware: DeminoHandler = (
 		req: Request,
 		_i: Deno.ServeHandlerInfo,
 		_ctx: DeminoContext
 	) => {
-		// do nothing if can't say explicitly
-		if (flag === undefined) return;
-
 		const url = new URL(req.url);
+
+		// no-op if can't say explicitly or we are at the root
+		if (flag === undefined || "/" === url.pathname) {
+			return;
+		}
 
 		// add slash
 		if (flag && !url.pathname.endsWith("/")) {
 			url.pathname += "/";
+			options?.logger?.(`[trailingSlash] 301 -> '${url.toString()}'`); // debug
 			return new Response(null, {
 				status: 301,
 				headers: { Location: url.toString() },
@@ -33,6 +42,7 @@ export function createTrailingSlashMiddleware(flag: boolean): DeminoHandler {
 		// remove slash
 		else if (!flag && url.pathname.endsWith("/")) {
 			url.pathname = url.pathname.slice(0, -1);
+			options?.logger?.(`[trailingSlash] 301 -> '${url.toString()}'`);
 			return new Response(null, {
 				status: 301,
 				headers: { Location: url.toString() },
