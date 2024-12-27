bc187c0c33bf384b3a84e5ec19aba2acba08f19e
Marian Meres <marian@meres.sk>
2024-12-27 13:41:29 +0100

    export fix

--- src/mod.ts
+++ src/mod.ts
@@ -1,4 +1,5 @@
 export * from "./demino.ts";
+export * from "./middleware/mod.ts";
+export * from "./misc/mod.ts";
 export * from "./router/mod.ts";
 export * from "./utils/mod.ts";
-export * from "./misc/mod.ts";
